e5c81bef715eb6b31aea6cb945c42d6ba5d852e5
Daisuke HIGASHI <d.higashi@atomitech.jp>
2019-02-12 13:17:58 +0900

    code

---
+++ src/js/code.js
@@ -0,0 +1,15 @@
+var codeBlocks = document.getElementsByClassName('code');
+[].forEach.call(codeBlocks, function(e) {
+  if (!/lang/.test(e.className)) {
+      return;
+  }
+  var lines = e.innerHTML.split(/\n/);
+  var codeBlock = "";
+  lines.forEach(function(line){
+    if(line != ""){
+      codeBlock += '<div class="code-line">' + line + '</div>'      
+    }
+  })
+  e.innerHTML = codeBlock;
+});
+
